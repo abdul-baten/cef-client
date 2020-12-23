8af52dd787123eb588e5be7cc148a8e999dfa6a0
Abdul Baten <md.abdul@brainstation23.com>
2020-12-23 23:32:41 +0600

    initialized project

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: []
+})
+export class AppModule {}
